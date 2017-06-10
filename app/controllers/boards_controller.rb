a08f2e3af40e9fd5f65dbb86df692765003f1f93
[01;31m[K"name"[m[K: "Nick <[01;31m[K"email"[m[K: null>
2017-06-10 11:40:10 -0700

    add all the controller files and layed out the psudo code routes

---
+++ app/controllers/boards_controller.rb
@@ -0,0 +1,8 @@
+# show
+get "/" do
+  erb :"index"
+end
+
+# index page for viewing all boards with a subscribe board
+# new for making board
+# show for individual board and it's jobs
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,3 @@
+
+# create user
+# view profile with subscribed boards
