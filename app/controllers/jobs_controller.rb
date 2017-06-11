360b0aa4609894c987bbd1680ed86350310f6c1c
[01;31m[K"name"[m[K: "Nick <[01;31m[K"email"[m[K: null>
2017-06-10 22:40:03 -0700

    add all the routes for job session and users

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,4 +1,26 @@
+# create new Job
+get '/jobs/:id/new' do
+  @board = Board.find(params[:id])
+  erb :'jobs/new'
 
+end
+post '/jobs' do
 
-# create a job for a particular board
+  @job = Job.new(params[:job])
+
+  if @job.save
+    redirect "/boards/#{@board.id}"
+  else
+    erb :'jobs/new'
+  end
+
+end
+
 # view a job posting
+get '/jobs/:id' do
+
+  @job = Job.find(params[:id])
+
+  erb :'jobs/show'
+
+end
