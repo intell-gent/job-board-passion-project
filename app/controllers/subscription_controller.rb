740d9b7dda7064b2101ac7bc1294c8b9f7bb18a5
Nick <email": null>
2017-06-16 00:04:26 -0700

    add the last finishing touches on my passion project

--- app/controllers/subscription_controller.rb
+++ app/controllers/subscription_controller.rb
@@ -2,12 +2,15 @@ post '/subscriptions/:board_id' do
   if logged_in?
     @board = Board.find(params[:board_id])
     @user = User.find_by(id: session[:id])
-    if current_user?(@board.hosting)
-      redirect :"boards/#{@board.id}"
-    else
+    # if current_user?(@board.hosting)
+    #   redirect :"boards/#{@board.id}"
+    # else
+
+    # Put subscription logic back here after testing
+
+    # end
       Subscription.create({board_id: params[:board_id], user_id: @user.id})
       redirect :"boards/#{@board.id}"
-    end
   else
     redirect :'sessions/new'
   end
@@ -16,12 +19,15 @@ delete '/subscriptions/:board_id' do
   if logged_in?
     @board = Board.find(params[:board_id])
     @user = User.find_by(id: session[:id])
-    if current_user?(@board.hosting)
-      redirect :"boards/#{@board.id}"
-    else
+    # if current_user?(@board.hosting)
+    #   redirect :"boards/#{@board.id}"
+    # else
+
+    # Put subscription logic back here after testing
+
+    # end
       Subscription.find_by({board_id: params[:board_id], user_id: @user.id}).destroy!
       redirect :"boards/#{@board.id}"
-    end
   else
     redirect :'sessions/new'
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,22 +4,15 @@ password = "passwordz"
 5.times do
   Category.create({name: Faker::Hipster.word})
 end
-2.times do
-  User.create({username: Faker::Internet.user_name, password: password})
+1.times do
+  User.create({username: ENV["MY_UNAM"], password: ENV["MY_PWRD"], email: ENV["MY_EMAIL"]})
 end
 
-# 7.times do
-#   Board.create({title: Faker::Hipster.word, description: Faker::Hipster.sentence, category_id: Category.all.sample.id, hosting_id: User.all.sample.id })
-# end
+6.times do
+  Board.create({title: Faker::Hipster.word, description: Faker::Hipster.sentence, category_id: Category.all.sample.id, hosting_id: User.all.sample.id })
+end
 
-# 4.times do
-#   user = User.all.sample.id
-#   board = Board.all.sample.id
 
-#     Subscription.create({user_id: user, board_id: board})
-
-# end
-
-30.times do
+15.times do
   Job.create({title: Faker::Hipster.word, position: Faker::Hipster.word, description: Faker::Hipster.paragraph, board_id: Board.all.sample.id, jobposting_id: User.all.sample.id })
 end
